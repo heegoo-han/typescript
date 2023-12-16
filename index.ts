81d72a272b5c2fe5e13b69acd9bd58069d7ca5aa
Heegoo Han <heegoo.han@lge.com>
2023-12-16 10:44:38 +0900

    npm init -y

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
+// when creating a github repository, typescript
+// I selected .gitignore type as "node"
+
 function sum(a: number, b: number): number {
     return a + b;
 }
 console.log(sum(10,20));
+
+// open terminal and then
+// ~/src/typescript > npm init -y
+// it will make package.json
